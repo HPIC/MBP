76c197f00ef8a9de1782d768942000d495ebd1a6
phw <xypiao97@korea.ac.kr>
2021-08-13 23:11:30 +0900

    re-apply

--- examples/test_wrap_dataloader.py
+++ examples/test_wrap_dataloader.py
@@ -6,25 +6,19 @@ import sys
 import math
 sys.path.append('../')
 
-from benchmark.data.dataset import cyclegan_dataset
+from benchmark.cyclegan.dataloader import cyclegan_dataset
 from mbs.micro_batch_streaming import MicroBatchStreaming
 
 def test_cyclegan_dataset():
     batch_size = 128
     mbs = MicroBatchStreaming(micro_batch_size=4)
-    dataloader = cyclegan_dataset('../benchmark/data/horse2zebra/train', image_size=256, batch_size=batch_size)
-    dataloader = mbs.dataloader(dataloader)
+    dataloader = cyclegan_dataset('../benchmark/cyclegan/dataset/horse2zebra/train', image_size=256, batch_size=batch_size)
+    dataloader = mbs.set_dataloader(dataloader)
 
-    for idx, (up, data0, data1) in enumerate(dataloader):
-        print(idx+1)
-        print(up, data0.size(), data1.size())
-
-def test_mnist_dataset():
-    pass
+    for idx, (ze, up, (data0, data1, _, _)) in enumerate(dataloader):
+        print(idx+1, ze, up, data0.size(), data1.size())
 
 if __name__ == '__main__':
     test_cyclegan_dataset()
     print('Check...OK!')
-    # test_mnist_dataset()
-    # print('Check...OK!')
 
