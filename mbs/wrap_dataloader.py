fbf5fb18744d32988543557fb977b8a7ba8ad6b8
xypiao97 <xypiao97@korea.ac.kr>
2021-08-28 12:01:36 +0000

    Re-design wrapping dataloader method, and re-apply

--- mbs/wrap_dataloader.py
+++ mbs/wrap_dataloader.py
@@ -4,36 +4,37 @@ import torch
 from torch.utils.data import DataLoader
 
 class MBSDataloader(DataLoader):
-    def __init__(self, micro_batch_size, *args, **kwargs) -> None:
-        super().__init__( *args, **kwargs )
-        self.mini_batch_size = self.batch_size
-        self.micro_batch_size = micro_batch_size
+    def __init__(
+        self,
+        target_dataloader : DataLoader,
+        micro_batch_size : int = None
+    ) -> None:
+        self.dataloader = target_dataloader._get_iterator() # Get iterator from dataloader by user.
+        self.mini_batch_size = target_dataloader.batch_size # Get batch size from dataloader by user.
+
+        # setting micro batch size
+        if micro_batch_size == None:
+            self.micro_batch_size = self.mini_batch_size
+        else:
+            self.micro_batch_size = micro_batch_size
 
     def chunk_dataset(self, dataset):
         rtn_dataset = []
         num_chunk = math.ceil( self.mini_batch_size / self.micro_batch_size )
 
-        if isinstance( dataset, dict ):
-            for i, name in enumerate(dataset):
-                if isinstance(dataset[name], torch.Tensor):
-                    if dataset[name].size(0) != self.mini_batch_size:
-                        num_chunk = math.ceil( dataset[name].size(0) / self.micro_batch_size )
-                    rtn_dataset.append(dataset[name].chunk( num_chunk ))
-                else:
-                    rtn_dataset.append(no_tensor_chunk(dataset[name], num_chunk))
-        elif isinstance( dataset, list ):
-            for data in dataset:
-                if isinstance(data, torch.Tensor):
-                    if data.size(0) != self.mini_batch_size:
-                        num_chunk = math.ceil( data.size(0) / self.micro_batch_size )
-                    rtn_dataset.append(data.chunk( num_chunk ))
-                else:
-                    rtn_dataset.append(no_tensor_chunk(data, num_chunk))
+        for _, data in enumerate(dataset):
+            data = dataset.get(data) if isinstance(dataset, dict) else data
+            if isinstance(data, torch.Tensor):
+                if data.size(0) != self.mini_batch_size:
+                    num_chunk = math.ceil( data.size(0) / self.micro_batch_size )
+                rtn_dataset.append(data.chunk( num_chunk ))
+            else:
+                rtn_dataset.append(no_tensor_chunk(data, num_chunk))
+
         return rtn_dataset, num_chunk
 
     def __iter__(self):
-        _dataloader = self._get_iterator()
-        for data in _dataloader:
+        for data in self.dataloader:
             micro_dataset, num_micro_batch = self.chunk_dataset(data)
             for midx in range(num_micro_batch):
                 zero = midx == 0
