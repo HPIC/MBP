9042ce10b56ba96272c7d3e7ff6916353674f1fb
xypiao97 <xypiao97@korea.ac.kr>
2021-08-29 12:14:01 +0000

    Add xception model for testing mbs

---
+++ benchmark/xception/trainer.py
@@ -0,0 +1,162 @@
+import itertools
+
+# import json
+import time
+from typing import List
+
+# Get dataset and model
+from dataloader import get_dataset
+from models.xception.xception import xception
+
+# PyTorch
+import torch
+from torch import nn
+from torch.optim.optimizer import Optimizer
+from torch.utils.data.dataloader import DataLoader
+from util.config_parser import ConfigParser, DotDict
+from util.util import ensure_dir, ensure_file_can_create, prepare_device
+
+# Get Micro-batch streaming.
+from mbs.micro_batch_streaming import MicroBatchStreaming
+
+
+class VGGTrainer:
+    def __init__(self, config: ConfigParser) -> None:
+        self.config = config
+
+    @classmethod
+    def _save_state_dict(cls, state_dict: dict, path: str) -> None:
+        ensure_file_can_create(path)
+        torch.save(state_dict, path)
+
+    @classmethod
+    def _save_log(cls, log, batch_size) -> None:
+        ensure_dir("./loss/")
+        # with open(f"./loss/cyclegan_mbs_{batch_size}_loss_value.json", "w") as file:
+        #     json_file = {}
+
+    @classmethod
+    def _get_data_loader(cls, dataset_config: DotDict) -> DataLoader:
+        dataloader = get_dataset(
+            path=dataset_config.path,
+            image_size=dataset_config.image_size,
+            batch_size=dataset_config.batch_size
+        )
+        return dataloader
+
+    @staticmethod
+    def _init_microbatch_stream(
+        dataloader: DataLoader, optim_list: List[Optimizer], micro_batch_size: int
+    ) -> List[Optimizer]:
+        # Define MicroBatchStreaming
+        mbs = MicroBatchStreaming()
+        dataloader = mbs.set_dataloader(dataloader, micro_batch_size)
+        for optim in optim_list:
+            optim = mbs.set_optimizer(optim)
+        return dataloader, optim_list
+
+    def _get_model_optimizer(self, device: torch.device, image_size: int) -> None:
+        # Define Models
+        self.xception_model = xception(
+            num_classes=self.config.data.dataset.num_class
+        ).to(device)
+
+        # Define loss function.
+        self.criterion = nn.CrossEntropyLoss().to(device)
+
+        # Define optimizers
+        self.opt = torch.optim.SGD( 
+            self.vgg_model.parameters(), 
+            lr=self.config.data.optimizer.lr,
+            momentum=self.config.data.optimizer.momentum,
+            weight_decay=self.config.data.optimizer.decay,
+        )
+
+    def train(self) -> None:
+        device, _ = prepare_device(target=self.config.data.gpu.device)
+        dataloader = self._get_data_loader(self.config.data.dataset.train)
+        self._get_model_optimizer(
+            device=device, image_size=self.config.data.dataset.train.image_size
+        )
+
+        optim_list = [self.opt]
+
+        if self.config.data.microbatchstream.enable:
+            dataloader, optim_list = self._init_microbatch_stream(
+                dataloader, optim_list, self.config.data.microbatchstream.micro_batch_size
+            )
+
+        self.loss_values = {}
+
+        for epoch in range(self.config.data.train.epoch):
+            self._train_epoch(epoch, dataloader, self.loss_values, device)
+
+        self._save_state_dict(
+            self.xception_model.state_dict(), "./parameters/xception/model.pth"
+        )
+
+    def _epoch_writer(
+        self,
+        epoch: int,
+        epochs: int,
+        train_time: float,
+        train_iter: int,
+        epoch_time: float,
+        epoch_iter: int,
+    ) -> None:
+        print(
+            f"[{epoch+1}/{epochs}]",
+            "train time :",
+            format(train_time / train_iter, ".3f") + "s",
+            "epoch time :",
+            format(epoch_time / epoch_iter, ".3f") + "s",
+            end=" ",
+        )
+        # json_file[epoch + 1] = {}
+        for _, name in enumerate(self.loss_values[epoch]):
+            print(
+                f"{name} :",
+                format(self.loss_values[epoch][name], ".2f"),
+                end=" ",
+            )
+        #     json_file[epoch + 1][name] = loss_values[epoch][name]
+        # print()
+        # json.dump(json_file, file, indent=4)
+
+    def _train_epoch(self, epoch, dataloader, loss_values, device) -> None:
+
+        # Define check performance vars
+        epoch_time = 0
+        epoch_iter = 0
+        train_time = 0
+        train_iter = 0
+
+        epoch_start = time.perf_counter()
+        loss_values[epoch] = {"loss": 0.0}
+        # pre_para = None
+        for idx, (ze, up, (image, label)) in enumerate(dataloader):
+            train_start = time.perf_counter()
+
+            input = image.to(device)
+            label = label.to(device)
+            output = self.xception_model( input )
+            loss = self.criterion( output, label )
+
+            self.opt.zero_grad_accu(ze)
+            loss.backward()
+            self.opt.step_accu(up)
+
+            train_end = time.perf_counter()
+            train_time += train_end - train_start
+            train_iter += 1 if up else 0
+            loss_values[epoch]["loss"] += loss.detach().item()
+        epoch_end = time.perf_counter()
+        epoch_time += epoch_end - epoch_start
+        epoch_iter += 1
+
+        loss_values[epoch]["loss"] /= idx
+
+
+def train(config: ConfigParser):
+    trainer = VGGTrainer(config)
+    trainer.train()
