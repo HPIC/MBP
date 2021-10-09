2dcbd3fdd08bc9a42b9117bc4f325dd63ed86814
xypiao97 <xypiao97@korea.ac.kr>
2021-10-09 12:07:27 +0900

    Before correct error

--- mbs/micro_batch_streaming.py
+++ mbs/micro_batch_streaming.py
@@ -1,5 +1,5 @@
 import math
-from typing import List, Optional
+from typing import List, Optional, Union
 import torch
 from torch.nn import Module
 from torch.optim import Optimizer
@@ -44,7 +44,7 @@ class _MBSBlock:
         self.micro_batch = micro_batch
         self.chunks = math.ceil( batch_size / micro_batch )
 
-class MicroBatchStreaming(_MBSBlock):
+class MicroBatchStreaming:
     def __init__(
         self,
         dataloader: DataLoader,
@@ -55,22 +55,24 @@ class MicroBatchStreaming(_MBSBlock):
         device_index: Optional[int] = None,
         micro_batch_size: int = 4,
     ) -> None:
-        super().__init__(
-            target_dev_idx=device_index,
-            batch_size=dataloader.batch_size,
-            micro_batch=micro_batch_size
-        )
+        # super().__init__(
+        #     target_dev_idx=device_index,
+        #     batch_size=dataloader.batch_size,
+        #     micro_batch=micro_batch_size
+        # )
         self.dataloader = dataloader
         self.module = model
         self.criterion = criterion
         self.optimizer = optimizer
         self.scheduler = lr_scheduler
 
-        self.data_queue = Queue()
-        self.loss_queue = Queue()
+        self.device = torch.device(f'cuda:{device_index}')
+
+        self.batch_size = dataloader.batch_size
+        self.micro_batch = micro_batch_size
+        self.chunks = math.ceil( self.batch_size / self.micro_batch )
 
     def train(self):
-        # with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prof:
         self.epoch_loss = 0
         for _, (data0, data1) in enumerate( self.dataloader ):
             data0: torch.Tensor
@@ -86,10 +88,9 @@ class MicroBatchStreaming(_MBSBlock):
 
             self.optimizer.zero_grad()
 
-            # with torch.cuda.device(self.device):
-            for _, (input, label) in enumerate( zip(chunk_data0, chunk_data1) ):
-                input = input.to(self.device)
-                label = label.to(self.device)
+            for _, (i, l) in enumerate( zip(chunk_data0, chunk_data1) ):
+                input = i.to(self.device)
+                label = l.to(self.device)
                 output: torch.Tensor = self.module( input )
                 loss: torch.Tensor = self.criterion( output, label ) / chunks
                 loss.backward()
@@ -97,23 +98,43 @@ class MicroBatchStreaming(_MBSBlock):
 
             self.optimizer.step()
             self.epoch_loss += mini_loss
-                # if idx > 1:
-                #     break
-        # prof.export_chrome_trace("trace.json")
 
     def get_loss(self):
         return self.epoch_loss / self.dataloader.__len__()
 
 
-def wait_stream(source: Stream, target: Stream):
-    source.wait_stream(target)
+def micro_batch_streaming(
+    dataloader: DataLoader,
+    model: Module,
+    criterion: Module,
+    optimizer: Optimizer,
+    lr_scheduler: _LRScheduler = None,
+    dev: Union[ torch.device, int ] = 0,
+    micro_batch_size: int = 4,
+):
+    epoch_loss = 0
+    for _, (data0, data1) in enumerate( dataloader ):
+        data0: torch.Tensor
+        data1: torch.Tensor
 
-@contextmanager
-def use_stream(strm: Stream):
-    with stream( strm ):
-        yield
+        mini_loss = 0
+        chunks = math.ceil( dataloader.batch_size / micro_batch_size )
+        if data0.size(0) != dataloader.batch_size:
+            chunks = math.ceil( data0.size(0) / micro_batch_size )
 
-class Task:
-    def __init__(self) -> None:
-        pass
+        chunk_data0 = data0.chunk( chunks )
+        chunk_data1 = data1.chunk( chunks )
 
+        optimizer.zero_grad()
+
+        for _, (i, l) in enumerate( zip(chunk_data0, chunk_data1) ):
+            input = i.to(dev)
+            label = l.to(dev)
+            output: torch.Tensor = model( input )
+            loss: torch.Tensor = criterion( output, label ) / chunks
+            loss.backward()
+            mini_loss += loss.item()
+
+        optimizer.step()
+        epoch_loss += mini_loss
+    return epoch_loss / len(dataloader)
