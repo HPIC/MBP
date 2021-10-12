ba023fbf48c0634cf1aa1ee176c174161e0f6d55
xypiao97 <xypiao97@korea.ac.kr>
2021-10-12 02:23:13 +0000

    Update file for experiment 2

--- benchmark/hw_resnet/train_exp2.sh
+++ benchmark/hw_resnet/train_exp2.sh
@@ -2,59 +2,73 @@
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 32 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 256 --micro_batch_size 64 \
+--exp 2 
+
+python main.py -m train -c toml/cifar10.toml \
+-r 50000000 -v 50 \
+-i 32 -d cifar10 --num_classes 10 \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 128 \
---exp 2 -w True
+--exp 2 
 
 # ResNet-50 with mbs true
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 32 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 256 --micro_batch_size 64 \
+--exp 2 
+
+python main.py -m train -c toml/cifar10.toml \
+-r 50000000 -v 50 \
+-i 32 -p True -d cifar10 --num_classes 10 \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 128 \
---exp 2 -w True
+--exp 2 
 
 #####################################################################
 
@@ -62,104 +76,132 @@ python main.py -m train -c toml/cifar10.toml \
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 152 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 152 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 152 \
 -i 32 -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 128 --micro_batch_size 32 \
+--exp 2 
+
+python main.py -m train -c toml/cifar10.toml \
+-r 50000000 -v 152 \
+-i 32 -d cifar10 --num_classes 10 \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 64 \
---exp 2 -w True
+--exp 2 
 
 # ResNet-152 with mbs true
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 152 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 152 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar10.toml \
 -r 50000000 -v 152 \
 -i 32 -p True -d cifar10 --num_classes 10 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 128 --micro_batch_size 32 \
+--exp 2 
+
+python main.py -m train -c toml/cifar10.toml \
+-r 50000000 -v 152 \
+-i 32 -p True -d cifar10 --num_classes 10 \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 64 \
---exp 2 -w True
+--exp 2 
 
 #####################################################################
 # ResNet-50 with mbs False
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 32 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 256 --micro_batch_size 64 \
+--exp 2 
+
+python main.py -m train -c toml/cifar100.toml \
+-r 50000000 -v 50 \
+-i 32 -d cifar100 --num_classes 100 \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 128 \
---exp 2 -w True
+--exp 2 
 
 # ResNet-50 with mbs true
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 32 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 50 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 256 --micro_batch_size 64 \
+--exp 2 
+
+python main.py -m train -c toml/cifar100.toml \
+-r 50000000 -v 50 \
+-i 32 -p True -d cifar100 --num_classes 100 \
+--num_workers 0  \
 --mbs True -b 256 --micro_batch_size 128 \
---exp 2 -w True
+--exp 2 
 
 #####################################################################
 
@@ -167,42 +209,56 @@ python main.py -m train -c toml/cifar100.toml \
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 152 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 152 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 152 \
 -i 32 -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 128 --micro_batch_size 32 \
+--exp 2 
+
+python main.py -m train -c toml/cifar100.toml \
+-r 50000000 -v 152 \
+-i 32 -d cifar100 --num_classes 100 \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 64 \
---exp 2 -w True
+--exp 2 
 
 # ResNet-152 with mbs true
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 152 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 8 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 152 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 16 \
---exp 2 -w True
+--exp 2 
 
 python main.py -m train -c toml/cifar100.toml \
 -r 50000000 -v 152 \
 -i 32 -p True -d cifar100 --num_classes 100 \
---num_workers 0 -s False \
+--num_workers 0  \
+--mbs True -b 128 --micro_batch_size 32 \
+--exp 2 
+
+python main.py -m train -c toml/cifar100.toml \
+-r 50000000 -v 152 \
+-i 32 -p True -d cifar100 --num_classes 100 \
+--num_workers 0  \
 --mbs True -b 128 --micro_batch_size 64 \
---exp 2 -w True
+--exp 2 
