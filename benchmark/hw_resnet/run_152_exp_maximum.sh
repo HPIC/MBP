a5b3aeac37db28f7d75e15f88378f981f9145e22
phw <xypiao97@korea.ac.kr>
2022-03-12 15:04:32 +0900

    UAdd script file for training

---
+++ benchmark/hw_resnet/run_152_exp_maximum.sh
@@ -0,0 +1,24 @@
+SEED='10000 1000000 500000 5000000 900000000'
+TOML_FILE='./toml/flower.toml'
+BATCH_SIZE='8 32 128 256'
+MICRO_BATCH='8'
+MODEL_VERSION='152'
+
+sed -i 's/version=1/version='"$MODEL_VERSION"'/' $TOML_FILE;
+sed -i 's/micro_batch_size=1/micro_batch_size='"$MICRO_BATCH"'/' $TOML_FILE;
+
+# MBS
+sed -i 's/eable=false/enable=true/' $TOML_FILE;
+for b in $BATCH_SIZE; do
+    sed -i 's/train_batch=1/train_batch='"$b"'/' $TOML_FILE;
+    for i in $SEED; do
+        sed -i 's/seed=1/seed='"$i"'/' $TOML_FILE;
+        python main.py -m train -c $TOML_FILE
+        sed -i 's/seed='"$i"'/seed=1/' $TOML_FILE;
+    done
+    sed -i 's/train_batch='"$b"'/train_batch=1/' $TOML_FILE;
+done
+sed -i 's/eable=true/enable=false/' $TOML_FILE;
+
+sed -i 's/micro_batch_size='"$MICRO_BATCH"'/micro_batch_size=1/' $TOML_FILE;
+sed -i 's/version='"$MODEL_VERSION"'/version=1/' $TOML_FILE;
